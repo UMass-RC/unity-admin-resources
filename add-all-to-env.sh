f757d7c11a8bb3dc103be1bc910f92548362b083
simonleary_umass_edu <simonleary_umass_edu@login2.unity.rc.umass.edu>
2022-08-19 18:06:24 +0000

    add script

---
+++ add-all-to-env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# source this to add the admin resources to your environment
+export PATH=$PATH:/modules/unity-admin-resources/misc
+export PATH=$PATH:/modules/unity-admin-resources/slurm
+source /modules/unity-admin-resources/modules-aliases
