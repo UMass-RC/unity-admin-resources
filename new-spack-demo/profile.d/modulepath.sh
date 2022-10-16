c0379ca92dd197e80fed6c83f134b56816342770
simonleary-umass-edu <simonleary@umass.edu>
2022-10-16 02:52:24 +0000

    cleanup redundant code

--- new-spack-demo/profile.d/modulepath.sh
+++ new-spack-demo/profile.d/modulepath.sh
@@ -3,23 +3,7 @@
 # this tells modified `module` function the location of readme file to print
 export LMOD_LIMITED_README=/modules/admin-resources/new-spack-demo/lmod-limited-view-printme.txt
 
-# for some reason if we use the spack-latest symlink lmod has a stack overflow
 SPACK_MOD_PREFIX="/modules/spack-latest/share/spack/lmod"
-#SPACK_MOD_PREFIX="/modules/spack-0.18.1/share/spack/lmod"
-
-#DEFAULT_DIRS=(
-#   "/modules/modulefiles"
-#)
-
-#COMPILER_DIRS=(
-#    "gcc-9.4.0"
-#    "intel/2021.4.0"
-#)
-
-#PROVIDER_DIRS=(
-#   "intel-oneapi-mpi/2021.6.0-h3cppyo"
-#    "openmpi/4.1.3-s3qn45p"
-#)
 
 yes_or_no() {
     case $1 in
@@ -67,21 +51,13 @@ for dir in ${DEFAULT_DIRS[@]}; do
 done
 
 IFS=$'\n'
-if [[ $LMOD_ENABLE_LIMITED_VIEW ]]; then
+if yes_or_no $LMOD_ENABLE_LIMITED_VIEW; then
     for arch in $(tac /etc/spack-cpu-arches); do
         prepend_modulepath "$SPACK_MOD_PREFIX/$arch/gcc-9.4.0"
     done
 else
-#    for arch in $(tac /etc/spack-cpu-arches); do
-#        for comp_dir in ${COMPILER_DIRS[@]}; do
-#            prepend_modulepath $SPACK_MOD_PREFIX/$arch/$comp_dir
-#            for prov_dir in ${PROVIDER_DIRS[@]}; do
-#                prepend_modulepath $SPACK_MOD_PREFIX/$arch/$prov_dir/$comp_dir
-#            done
-#        done
-#    done
     for dir in $(/modules/admin-resources/production/bin/unity-module-hierarchy); do
-        prepend_modulepath $dir
+        prepend_modulepath $SPACK_MOD_PREFIX/$dir
     done
 fi
 unset IFS
